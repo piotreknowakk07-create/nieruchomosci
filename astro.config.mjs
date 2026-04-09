45be8a8bbd8646741f257d5d0ebd07585604bc13
Piotr N <piotrek.nowakk07@gmail.com>
2026-04-09 17:53:36 +0200

    coookies + seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://kupimynieruchomosclodz.pl',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [
+    sitemap({
+      customPages: [
+        'https://kupimynieruchomosclodz.pl/',
+        'https://kupimynieruchomosclodz.pl/privacy.html',
+      ],
+    }),
+  ],
 });
